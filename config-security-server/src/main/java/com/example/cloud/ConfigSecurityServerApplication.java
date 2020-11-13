6a9e517b4a4018352d00f6619386394ac9f476ef
wangjintianya <601221052@qq.com>
2020-11-13 17:10:54 +0800

    Config：外部集中化配置管理

---
+++ config-security-server/src/main/java/com/example/cloud/ConfigSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSecurityServerApplication.class, args);
+    }
+
+}
